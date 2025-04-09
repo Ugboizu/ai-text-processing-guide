143a5b011efd062631fde643b5e1bfd8a1604f5e
Ugboizu <umezugbo@gmail.com>
2025-04-09 02:37:21 +0100

    font changes to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,19 +11,19 @@ export default function Home() {
         </div>
 
 		<div className='absolute top-1/2 left-1/2 text-center translate-x-[-50%] translate-y-[-50%] text-white'>
-			<h1 className='font-bold text-5xl py-10'>ViolaAI</h1>
+			<h1 className='font-bold font-underdog text-5xl py-10'>ViolaAI</h1>
 
 			<div className="max-w-lg mx-auto">
 				<Link to="/chat" className="block">
 					<button
-					className="w-full bg-blue-700 text-white text-xl font-semibold py-4 px-6 rounded-lg shadow-lg 
+					className="w-full bg-blue-700 text-white text-xl font-roboto font-semibold py-4 px-6 rounded-lg shadow-lg 
 						hover:bg-blue-800 transition duration-300 ease-in-out transform hover:scale-105">
 					Get Started
 					</button>
 				</Link>
-				<p className="mt-4 text-lg text-gray-200">
+				<p className="mt-4 text-base font-medium text-gray-200 font-underdog italic">
 					Instantly detect languages, translate, and summarize paragraphs with Voila AI. 
-					Perfect for texts over 150 words—try it now!
+					Perfect for texts over 150 characters — Try it now!
 				</p>
 			</div>
 		</div>
